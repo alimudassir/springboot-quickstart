71367fb6a5305f3af808d933f6eb33abde958e51
Mohammed Mudassir Ali <alimudassir12@gmail.com>
2018-07-01 20:58:48 +0530

    Added Controller to display message

---
+++ src/main/java/com/generic/springbootstarter/GenericSpringApp.java
@@ -0,0 +1,14 @@
+package com.generic.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericSpringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(GenericSpringApp.class, args);
+	}
+
+}
